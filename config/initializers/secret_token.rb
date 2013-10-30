4d6283f207b0f94dfb3c3199651c67c72dc32721
Apprentice <apprentice@dbc-22.local>
2013-10-30 11:10:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tech2me::Application.config.secret_token = '4c8bdb315d8b38061681351a41a63c6e74fa1e785ee2dec020a3094a78612b012f335a6c8d6dd9396f852067d12c0c655843a42c09884551994592fe8895c5d2'
