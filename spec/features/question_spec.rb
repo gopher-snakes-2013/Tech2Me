8d686537e4449f767b5d281c9e8b409b930c87fe
Dev Bootcamp <Apprentice@dbc33.local>
2013-11-01 15:41:22 -0700

    added capybara test coverage for pop up form

--- spec/features/question_spec.rb
+++ spec/features/question_spec.rb
@@ -8,4 +8,21 @@ feature "Questions" do
       expect(page).to have_content("Burger King")
     end
   end
+
+  context "User clicks 'New Question' button" do
+    it "should display a pop up form" do
+      visit questions_path
+      click_on 'New Question'
+      expect(page).to have_content("Ask a Question:")
+    end
+
+    it "should create a new question when you fill in the form and submit" do
+      visit questions_path
+      click_on 'New Question'
+      fill_in 'question_title', :with => 'Trostli Inc'
+      fill_in 'question_body', :with => 'What is the square root of Daniel?'
+      click_on 'Ask dat question'
+      expect(page).to have_content('Trostli Inc')
+    end
+  end
 end
