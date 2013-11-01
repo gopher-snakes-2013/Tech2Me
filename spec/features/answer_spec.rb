ed2a4b8dde6847edca038fe65a729ba38d7fe4aa
Robert Taraya <anniesing1014@gmail.com>
2013-10-31 17:49:50 -0700

    refactor tests and remove comments

--- spec/features/answer_spec.rb
+++ spec/features/answer_spec.rb
@@ -4,24 +4,20 @@ require 'spec_helper'
 feature "when user visits new page" do
 
   let(:my_question){
-    Question.new(title: "Is this a valid question?", body: "let's take this question seriously." )
+    Question.create(title: "Is this a valid question?", body: "let's take this question seriously." )
   }
 
     it "page shows Add a new answer" do
-      # visit question_path(my_question)
-      # page.should have_content('Add a new answer')
+      visit question_path(my_question)
+      page.should have_content('Add a new answer')
     end
 
     it 'page shows form that redirects to question#show' do
-      # visit question_path(my_question)
-      # puts page.body
-      # fill_in('answers_author', :with => 'Dan Authorman')
-      # fill_in('answers_answer', :with => 'I am an answer')
-      # click_on('Save Answers')
-      # page.should have_content('I am an answer')
+      visit question_path(my_question)
+      fill_in('answer_author', :with => 'Dan Authorman')
+      fill_in('answer_answer', :with => 'I am an answer')
+      click_on('Create Answer')
+      page.should have_content('I am an answer')
     end
 
 end
-      # expect {
-      #   post :create, params, (answers, :count)
-      #   }.by(1)
