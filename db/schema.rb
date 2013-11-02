490083c85c23fde49775b9375969d54bd4866692
Dev Bootcamp <Apprentice@dbc34.local>
2013-11-02 16:03:14 -0700

    able to create new users login, and logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131031184452) do
+ActiveRecord::Schema.define(:version => 20131101230340) do
 
   create_table "answers", :force => true do |t|
     t.string  "author"
@@ -25,5 +25,16 @@ ActiveRecord::Schema.define(:version => 20131031184452) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string "email"
+    t.string "password"
+    t.string "encrypted_password", :limit => 128
+    t.string "confirmation_token", :limit => 128
+    t.string "remember_token",     :limit => 128
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
